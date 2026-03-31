14b555385152a7d140d6c49cee6e0d6ad9494696
Mayank <imayank1724@gmail.com>
2026-04-01 02:50:11 +0530

    chat history created improved performance of llm

--- LangchainPrompts/chatbot.py
+++ LangchainPrompts/chatbot.py
@@ -6,11 +6,14 @@ api_key = os.getenv("OPENROUTER_API_KEY")
 model = ChatOpenAI(api_key=api_key, 
                    base_url="https://openrouter.ai/api/v1",
                    model='openai/gpt-4o-mini')
-
+chat_history = []
 while True:
     user_input = input("ME: ")
+    chat_history.append(user_input)
     if user_input.lower() == 'exit':
         print("Exiting the chatbot. Goodbye!")
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
     print("Chatbot Response:", result.content)
+print("Chat history:", chat_history)
