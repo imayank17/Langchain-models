656e23c1d0860e745e9ca50af7ce70e5eb7a9f99
Mayank <imayank1724@gmail.com>
2026-04-01 15:33:34 +0530

    annotated

--- structured_output/1.sentiment_analysis.py
+++ structured_output/1.sentiment_analysis.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict,Annotated,Optional
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,9 +7,24 @@ api_key = os.getenv("OPENROUTER_API_KEY")
 model=ChatOpenAI(api_key=api_key, base_url="https://openrouter.ai/api/v1",model='openai/gpt-4o-mini')
 
 class SentimentInput(TypedDict):
-    summary: str
-    sentiment: str
-
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A concise summary of the input text"]
+    sentiment: Annotated[str, "The overall sentiment of the input text, categorized as Positive, Negative, or Neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
 structured_input = model.with_structured_output(SentimentInput)
-result = structured_input.invoke("Best Bollywood movie of the year. It was absolutely brilliant. I really enjoyed all of it, the acting, cinematography, writing, pace, suspense, comedy, gore, music, soundtrack and much more. Love original movies like this. More of this please! Ho, mainu lutt le gaya")
+result = structured_input.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Nitish Singh
+""")
 print(result)
